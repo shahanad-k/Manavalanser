eef5bb2109ec0f8b5afe0793c8bd404e189e9d09
Shahanad k  (MANAVALAN) <90305912+shahanad-k@users.noreply.github.com>
2021-09-17 12:05:34 +0530

    Create git.js

---
+++ plugins/git.js
@@ -0,0 +1,36 @@
+const Asena = require('../events');
+const {MessageType, MessageOptions, Mimetype} = require('@adiwajshing/baileys');
+const axios = require('axios');
+
+const Language = require('../language');
+const Lang = Language.getString('wallpaper');
+
+Asena.addCommand({pattern: 'git', fromMe: false, desc: Lang.WP}, (async (message, match) => {
+
+    var r_text = new Array ();
+    
+    
+   
+  r_text[0] = "https://i.imgur.com/UCCrYk1.jpeg";
+    
+    
+    var i = Math.floor(1*Math.random())
+
+    var respoimage = await axios.get(`${r_text[i]}`, { responseType: 'arraybuffer' })
+
+    await message.sendMessage(Buffer(respoimage.data), MessageType.image, {mimetype: Mimetype.png, caption: `*MANAVALAN*
+
+*owner number http://wa.me/+919645090649?text=Hi*
+
+
+*paathu githublink   https://github.com/shahanad-k/Manavalanser_*
+
+
+*https://www.instagram.com/*
+
+
+
+`}) 
+
+}));
+        
